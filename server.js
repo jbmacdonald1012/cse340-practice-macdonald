858f7a65db0cd9b2bcf6e424e96c92b4a3f41d11
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2026-01-14 22:11:06 -0700

    Unit 1 work so far

---
+++ server.js
@@ -0,0 +1,30 @@
+﻿import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.set('view engine', 'ejs');
+
+// app.set('views', path.join(__dirname, 'src/views'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+app.get('/products', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
